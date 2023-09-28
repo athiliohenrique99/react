5c9f242f53fd1b8f0ebf03c9f0c1b44deddaaa5a
Athilio <athiliohenrique98@gmail.com>
2023-09-27 22:50:49 -0300

    react

--- secretword/src/components/Game.js
+++ secretword/src/components/Game.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import './Game.css'
 
 const Game = ({
@@ -11,6 +11,19 @@ const Game = ({
   guesses,
   score
 }) => { 
+const [letter, setLetter] = useState("");
+const letterInputRef = useRef(null);
+
+const handleSubimit = (e) => {
+  e.preventDefault();
+
+  verifyLetter(letter)
+
+  setLetter("");
+
+  letterInputRef.current.focus();
+}
+
   return (
     <div className='game'>
       <p className='points'>
@@ -36,8 +49,11 @@ const Game = ({
       </div>
       <div className="letterContainer">
         <p>Tente advinhar uma letra da palavra: </p>
-        <form>
-          <input type="text" name='letter' maxLength='1' required />
+        <form onSubmit={handleSubimit}>
+          <input type="text" name='letter' maxLength='1' required onChange={(e) => setLetter(e.target.value)}
+          value={letter}
+          ref={letterInputRef}
+          />
           <button>Jogar!</button>
         </form>
       </div>
