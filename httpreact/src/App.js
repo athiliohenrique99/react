fab9e38a5c57b98db887ac388b8432982afc6172
Athilio <athiliohenrique98@gmail.com>
2023-10-08 23:21:16 -0300

    react

--- httpreact/src/App.js
+++ httpreact/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {useState, useEffect} from 'react';
+
+const url = "http://localhost:3000/products"
+
 function App() {
+const [products, setProducts] =useState([])
+
+// 1 - resgatando dados
+useEffect(async() => {
+
+const res = await fetch(url)
+
+});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lista de produtos</h1>
     </div>
   );
 }
