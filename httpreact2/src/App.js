9eb0ad19baa7fe129e808202b5d3ffa4130ff2e8
Athilio <athiliohenrique98@gmail.com>
2023-10-19 23:59:21 -0300

    react

--- httpreact2/src/App.js
+++ httpreact2/src/App.js
@@ -8,18 +8,29 @@ function App() {
   const [products, setProducts] = useState([])
 
   // 1 - resgatando dados
-  useEffect(async () => {
+  useEffect(() => {
+    async function fetchData() {
 
-    const res = await fetch(url);
+      const res = await fetch(url);
+      const data = await res.json();
+      setProducts(data);
+    }
 
-    const data = await res.json();
 
-    setProducts(data);
 
+
+    fetchData();
   }, []);
 
-  console.log(products);
 
+  // }, []);
+
+
+
+  // }, []);
+
+  console.log(products);
+  console.log('aqui');
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
